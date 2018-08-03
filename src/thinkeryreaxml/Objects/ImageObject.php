cd18e0b0ba7ee1e91c543eef2914bfb83639ff22
James Orland <james@precedence.com.au>
2018-08-03 18:26:31 +1000

    Handle image IDs greater than z (ie aa, ab)

--- src/thinkeryreaxml/Objects/ImageObject.php
+++ src/thinkeryreaxml/Objects/ImageObject.php
@@ -6,21 +6,24 @@ class ImageObject extends MediaMember
 {
     public function setOrdering($ordering)
     {
-        // they use m for the main picture, then revert to a-z for others. Why? Who knows.
-        if ($ordering == 'm') {
-            $order = 1;
-        } else {
-            $order = $this->getIntFromLetter($ordering);
-        }
-        $this->ordering = $order;
+        $this->ordering = $this->getIntFromLetters($ordering);
         return $this;
     }
 
-    private function getIntFromLetter($order)
-    {
-        $alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','n','o','p','q','r','s','t','u','v','w','x','y','z'];
-        $raw_value = array_search($order, $alphabet);
-
-        return $raw_value + 2; // add 2, since m is always 1 and array indexing starts at zero
+    public static function getIntFromLetters($id) {
+        $id = strtoupper($id);
+        $val=0;
+        for($i = 0; $i < strlen($id); $i++) {
+            $delta = ord($id[$i]) - 64;
+            $val = $val*26+$delta;
+        }
+        // they use m for the main picture, then revert to a-z for others. Why? Who knows.
+        if($val==13) {
+            $val=1;
+        }
+        else if($val<13) {
+            $val+=1;
+        }
+        return $val;
     }
 }
